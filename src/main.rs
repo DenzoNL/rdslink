b4477c3d9927b6f9066ba6c64e07b2ff3cbf2364
Dennis Bogers <d.bogers@korsit.com>
2025-05-22 17:59:47 +0200

    feat: implement fetching profile names from config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use aws_config::profile;
+use tokio; // For async main
+
+#[tokio::main]
+async fn main() {
+    let profiles = get_aws_profiles().await;
+    println!("AWS Profiles:");
+    for profile in profiles {
+        println!("{}", profile);
+    }
+}
+
+async fn get_aws_profiles() -> Vec<String> {
+    let profile_set = profile::load(
+        &Default::default(),
+        &Default::default(),
+        &Default::default(),
+        None,
+    )
+    .await
+    .unwrap();
+
+    profile_set.profiles().map(ToString::to_string).collect()
 }
